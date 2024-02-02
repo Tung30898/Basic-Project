d1e5b63d848c44ae8862b116ed1983f5be74ea74
tung30898 <phamtung30898@gmail.com>
2024-02-02 23:46:50 +0700

    Project: Register & login Desciption: There are 3 selection Register, Login, Exit. User want to creat a account The system will save information to .txt file When user Login The system will check account & password to decide Whether, This account is exist.

---
+++ register.cpp
@@ -0,0 +1,72 @@
+#include"register.h"
+
+using namespace std;
+
+int Register::SelectionScreen() {
+    int select = 0;
+    cout << "1.Sign up\n";
+    cout << "2.Login\n";
+    cout << "3.Exit\n";
+    cout << "Your Selection: ";
+    cin >> select;
+    return select;
+}
+
+void Register::CreatAcc() {
+    string nameAcc;
+    string pws, repws;
+    fstream write;
+    cout << "Account name:";
+    cin >> nameAcc;
+    cout << "Password:";
+    cin >> pws;
+    do {
+        cout << "Re-Password:";
+        cin >> repws;
+    }while(pws != repws);
+    write.open("test.txt",ios::app | ios::ate);
+    if(!write.is_open()) {
+        cout << "File opened failed";
+        return;
+    }
+    
+    write<<nameAcc;
+    write<<endl;
+    write<<pws;
+    write<<endl;
+    write.close();
+}
+
+void Register::Login() {
+    int count = 0;
+    string acc, pws;
+    string accSave, pwsSave;
+    fstream read("test.txt", ios::in);
+    cout << "Account: ";
+    cin >> acc;
+    cout << "Password: ";
+    cin >> pws;
+    while(!read.eof()) {
+        switch(count) {
+            case 0:
+                read>>accSave;
+                cout << accSave << "\n";
+                count++;
+                break;
+            case 1:
+                read>>pwsSave;
+                cout << accSave << "\n";
+                count++;
+                break;
+            case 2:
+                count = 0;
+                if(accSave == acc && pwsSave == pws) {
+                    cout << "Login successful";
+                    read.close();
+                    return;
+                }
+                break;
+        }
+    }
+    read.close();
+}
